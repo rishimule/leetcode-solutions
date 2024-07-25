8aedd415c1acd7e00513784bd5a9f369851c3cfa
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2024-07-24 20:37:29 -0400

    Time: 56 ms (23.07%), Space: 16.9 MB (77.83%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        h = {}
+        for l in s:
+            if l in h:
+                h[l] += 1
+            else:
+                h[l] = 1
+        
+        for l in t:
+            if l in h:
+                if h[l] > 0:
+                    h[l] -= 1
+                else:
+                    return False
+            else:
+                return False
+        
+        if sum(h.values()) == 0:
+            return True
+        else:
+            return False
