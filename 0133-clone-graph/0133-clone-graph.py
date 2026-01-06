e8441f1d399f17e5f2bdaba1c683a7d15fc9fd67
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-01-06 16:55:12 -0500

    Time: 41 ms (68.56%), Space: 19.9 MB (11.13%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,39 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node == None:
+            return None
+
+        nodeDict = {}
+        parsednodeset = set()
+
+        def parsenode(node):
+            newNode = Node(node.val, [])
+            nodeDict[node.val] = newNode
+            parsednodeset.add(node.val)
+
+
+            for n in node.neighbors:
+                if n.val not in parsednodeset:
+                    neighnode = parsenode(n)
+                    newNode.neighbors.append(neighnode)
+                else:
+                    newNode.neighbors.append(nodeDict[n.val])
+            
+            return newNode
+
+        return parsenode(node)
+
+
+
+
+    
+        
