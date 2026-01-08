1ba21620c84dfdf93ba26b780c0e9558cd3292b7
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-01-08 01:38:05 -0500

    Time: 119 ms (7.74%), Space: 29.6 MB (16.39%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,25 +1,24 @@
-from typing import List
-
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+
         n = len(height)
-        
-        l = 0
-        r = n-1
-        
-        max_area = 0
-        
-        while l < r:
-            max_area = max(max_area, min(height[l], height[r]) * (r-l))
-            
-            if height[l] <= height[r]:
-                l += 1
+
+        #greedy
+        lp  = 0
+        rp = n-1
+
+        maxArea = 0
+
+        while lp < rp:
+            currArea = (rp - lp) * min(height[lp], height[rp])
+            maxArea = max(maxArea, currArea)
+
+            if height[lp] > height[rp]:
+                rp -= 1
             else:
-                r -= 1
-                
-        return max_area
-            
-    
-# s = Solution()
-# s.maxArea(height = [1,8,6,2,5,4,8,3,7]) # Output: 49
+                lp += 1
+        
+        return maxArea
+
+
+        
