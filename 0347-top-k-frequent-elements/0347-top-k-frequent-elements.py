3ceb01247d26c4eba154ec914e08818800882ce8
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-02-07 15:52:52 -0500

    Time: 4 ms (70.79%), Space: 22.9 MB (39.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,23 +1,17 @@
-from typing import List
-from collections import defaultdict
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        # Create a hashmap to store the number of occurrences
-        count = defaultdict(int)
-        
+        count = {}
+
         for n in nums:
-            count[n] += 1
+            count[n] = 1 + count.get(n, 0)
         
-        # Return k elements with max counts
-        res = list(count.items())
-        res.sort(key=lambda a: a[1], reverse=True)
-        print(res)
+        heap = []
+        for n in count.keys():
+            heapq.heappush(heap, (count[n], n))
+            if len(heap) > k:
+                heapq.heappop(heap)
         
-        return [x[0] for x in res[:k]]
-            
-            
-# s = Solution()
-# s.topKFrequent(nums = [4,1,-1,2,-1,2,3], k = 2)
+        res = [x[1] for x in heap]
+        return res
+
+        
