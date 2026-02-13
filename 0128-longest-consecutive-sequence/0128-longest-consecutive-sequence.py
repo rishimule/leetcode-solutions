ad891eeb613b99784bbdff16aa35f5010e3aed5c
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-02-12 20:26:40 -0500

    Time: 47 ms (68.93%), Space: 36.6 MB (61.99%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -12,18 +12,16 @@ class Solution:
         num_set = set(nums)
         max_len = 0
 
-        count = 0
-        for n in nums:
-            if n in num_set:
-                if n-1 not in num_set:
-                    # n is the start of the series:
-                    # start counting
-                    x = n
-                    while x in num_set:
-                        count += 1
-                        num_set.remove(x)
-                        x += 1
-                    max_len = max(max_len, count)
-                    count = 0
+        
+        for n in num_set:
+            if n-1 not in num_set:
+                # n is the start of the series:
+                # start counting
+                count = 0
+                while (n + count) in num_set:
+                    count += 1
+
+                max_len = max(max_len, count)
+                count = 0
             
         return max_len
