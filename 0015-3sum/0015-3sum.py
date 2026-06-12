1eec46a1b5dfc639c4a9e32339052172f60f7493
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-06-12 10:47:53 -0400

    Time: 584 ms (75.92%), Space: 22.3 MB (32.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,54 +1,29 @@
-from typing import List
-
-
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Sort the inputs : O(n^2)
-        nums.sort()
-
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort() # nLogn
+        res = [] 
         n = len(nums)
         
-        res = []
-        
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
                 continue
-            
-            a = nums[i]
-            bc_sum = 0 - a
-            
-            # Use two pointer approach for the right side of "a"
-            # Initialize left and right pointers
-            l = i + 1
-            r = n - 1
-            
-            while l < r:
-                if nums[l] + nums[r] > bc_sum:
-                    while True:
-                        r -= 1
-                        if nums[r] != nums[r+1] or l >= r:
-                            break
-                elif nums[l] + nums[r] < bc_sum:
-                    while True:
-                        l += 1
-                        if nums[l] != nums[l-1] or l >= r:
-                            break
-                else:
-                    res.append([nums[i], nums[l], nums[r]]) # append [a,b,c] to result
-                    # check if any other values for "b","c" exist for the current "a"
-                    while True:
-                        l += 1
-                        if nums[l] != nums[l-1] or l >= r:
-                            break  
-                    while True:
-                        r -= 1
-                        if nums[r] != nums[r+1] or l >= r:
-                            break
-            
-        return res
-            
-            
-            
+            #consider [i+1 ....... n-1]
+            l = i+1
+            r = n-1
 
-# s = Solution()
-# s.threeSum(nums = [-1,0,1,2,-1,-4]) # [[-1,-1,2],[-1,0,1]]
+            target = 0 - a
+
+            while l < r:
+                if nums[l] + nums[r] < target:
+                    l += 1
+                elif nums[l] + nums[r] > target:
+                    r -= 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        
+        return res
+
+        
