bc99c7a55e6be7aed97b23c9ea40469b9d613cd2
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-06-13 17:33:13 -0400

    Time: 5 ms (24.07%), Space: 19.4 MB (41.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,46 +1,52 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        
-        q = deque() # ---> (row, col, time)
+        rows = len(grid)
+        cols = len(grid[0])
+
+        q = collections.deque()
+
+        fresh = set()
+        m = 0
+
         visited = set()
-        time = 0
 
-        # find all rotten oranges
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 2:
-                    q.append((r, c, time))
-        
-        # do bfs ->  one level per minute
-        while q:
-            r, c, time = q.popleft()
-            visited.add((r, c))
-
-            neighbors = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-
-            for dr, dc in neighbors:
-                new_r = r + dr
-                new_c = c + dc
-                
-                if (0 <= new_r < ROWS  
-                    and 0 <= new_c < COLS
-                    and grid[new_r][new_c] == 1
-                    and (new_r, new_c) not in visited):
-
-                    grid[new_r][new_c] = 2
-                    q.append((new_r, new_c, time + 1))
-        
-        # Check if any fresh orange is left
-        for r in range(ROWS):
-            for c in range(COLS):
+                    q.append((r, c, m))
                 if grid[r][c] == 1:
-                    return -1
-
-        return time
-
-
+                    fresh.add((r, c))
         
+        
+        while q:
+            row, col , mr = q.popleft()
+            
+            directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+            for dr, dc in directions:
+                r = row + dr
+                c = col + dc
+
+                if ((r in range(rows))
+                    and (c in range(cols))
+                    and ((r,c) not in visited)
+                    and (grid[r][c] == 1)):
+
+                    # apple is spoiled
+                    grid[r][c] = 2
+                    q.append((r, c, mr + 1))
+                    fresh.remove((r, c))
+    
+            
+            visited.add((row, col))
+            m = max(m, mr)
+
+        if len(fresh) > 0:
+            return -1
+        else:
+            return m
+        
+
 
 
 
