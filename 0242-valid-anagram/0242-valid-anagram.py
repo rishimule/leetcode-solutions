187e607c32186b0d8b47ac60691942507fc97448
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-07-30 10:50:49 -0400

    Time: 11 ms (77.22%), Space: 19.5 MB (45.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,65 +1,27 @@
-from collections import Counter, defaultdict
-
+# from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # return sorted(s) == sorted(t) # Time: O(nLogn) ---- Space: O(1)
+        # return Counter(s) == Counter(t)
 
-        # return Counter(s) == Counter(t) # Time: O(n) ---- Space: O(n)
-
+        # edge cases
         if len(s) != len(t):
             return False
 
-        count_s = defaultdict(int)
-        count_t = defaultdict(int)
+        sCount = [0] * 26         
+        tCount = [0] * 26
 
         for i in range(len(s)):
-            count_s[s[i]] += 1
-            count_t[t[i]] += 1
+            idx = ord(s[i]) - ord("a")
+            sCount[idx] += 1
+
+            idx = ord(t[i]) - ord("a")
+            tCount[idx] += 1
         
-        return count_s == count_t
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # return Counter(s) == Counter(t)
+        for i in range(26):
+            if sCount[i] != tCount[i]:
+                return False
         
-        # return sorted(s) == sorted(t)
-        # # nUll check
-        # if len(s) != len(t):
-        #     return False
-
-        # # count = {}
-        
-        # # for i in range(len(s)):
-        # #     if s[i] not in count:
-        # #         count[s[i]] = 0
-        # #     count[s[i]] += 1
-        
-        # count_s = Counter(s)
-        # count_t = Counter(t)
-
-        # diff= 0
-        # for char in set (s + t):
-        #     diff += abs(count_s[char] - count_t[char])
-        
-        # return True if not diff else False
+        return True
 
 
-        
+
