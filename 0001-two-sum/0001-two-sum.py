b62333764aacf0c7f1f89fe7769faa89ffb75eda
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2026-07-30 10:56:03 -0400

    Time: 3 ms (53.82%), Space: 20.4 MB (57.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,23 @@
+"""
+[2,3,4,5,6] --- 11
+
+n + diff = target
+diff = target - n
+
+[9, 8,  7, 6, ]
+
+
+
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        hmap = {}
-        for i in range(n):
-            if target - nums[i] in hmap:
-                return [hmap[target - nums[i]], i]
-            else:
-                hmap[nums[i]] = i
+        diffIdx = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in diffIdx:
+                return [diffIdx[diff], i]
+            diffIdx[nums[i]] = i
+        
+        return None
